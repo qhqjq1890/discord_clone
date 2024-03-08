750583f2731b5952ce85affc96c619fa177456b8
김다솔 <gimdasol@gimdasol-ui-Macmini.local>
2024-03-08 23:07:35 +0900

    add chat scheme

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,4 +1,6 @@
 import { Hash } from "lucide-react";
+import MobileToggle from "@/components/mobile-toggle";
+import { UserAvatar } from "@/components/user-avatar";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -7,10 +9,20 @@ interface ChatHeaderProps {
   imageUrl?: string;
 }
 
-const ChatHeader = () => {
+const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
   return (
     <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
-      Chat Header!
+      <MobileToggle serverId={serverId} />
+      {type === "channel" && (
+        <Hash className="h-5 w-5 text-zinc-500 dark:text-zinc-500 mr-2" />
+      )}
+      {type === "conversation" && (
+        <UserAvatar
+          src={imageUrl}
+          className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2"
+        />
+      )}
+      <p className="font-semibold text-md text-black dark:text-white">{name}</p>
     </div>
   );
 };
